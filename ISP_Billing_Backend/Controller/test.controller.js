d31a8be1a157a57e54895c578177253afdaa8169
Nuyana <agenttabs@gmail.com>
2026-04-29 22:22:22 +0800

    update

---
+++ ISP_Billing_Backend/Controller/test.controller.js
@@ -0,0 +1,13 @@
+const User = require("../../Model/test.schema");
+
+// GET all users
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+};
+
+// CREATE user
+exports.createUser = async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+};
